d584e53fc312fe356786d26a0cdf068968cadb71
joonissi <joonanissinen123@gmail.com>
2018-11-22 21:54:21 +0200

    updated UI

--- Opinnot/src/main/java/opinnot/ui/AppUI.java
+++ Opinnot/src/main/java/opinnot/ui/AppUI.java
@@ -26,36 +26,35 @@ public class AppUI extends Application  {
     @Override
     public void start(Stage ikkuna) {
         
-        // 1. Luodaan salasanan kysymiseen käytetty näkymä
-
-        // 1.1 luodaan käytettävät komponentit
         Label ohjeteksti = new Label("Kirjoita käyttäjätunnus ja salasana kirjautuaksesi");
+        Label kayttajatunnusTeksti = new Label("Käyttäjätunnus:");
         TextField kayttajatunnuskentta = new TextField();
+        Label salasanaTeksti = new Label("Salasana:");
         PasswordField salasanakentta = new PasswordField();
         Button aloitusnappi = new Button("Kirjaudu");
+        Button rekisterointinappi = new Button("Rekisteröidy");
         Label virheteksti = new Label("");
 
-        // 1.2 luodaan asettelu ja lisätään komponentit siihen
         GridPane asettelu = new GridPane();
 
         asettelu.add(ohjeteksti, 0, 0);
-        asettelu.add(kayttajatunnuskentta, 0, 1);
-        asettelu.add(salasanakentta, 0, 2);
-        asettelu.add(aloitusnappi, 0, 3);
-        asettelu.add(virheteksti, 0, 4);
+        asettelu.add(kayttajatunnusTeksti, 0, 2);
+        asettelu.add(kayttajatunnuskentta, 0, 3);
+        asettelu.add(salasanaTeksti, 0, 4);
+        asettelu.add(salasanakentta, 0, 5);
+        asettelu.add(aloitusnappi, 0, 6);
+        asettelu.add(rekisterointinappi, 0, 7);
+        asettelu.add(virheteksti, 0, 8);
 
-        // 1.3 tyylitellään asettelua
-        asettelu.setPrefSize(500, 180);
+        asettelu.setPrefSize(500, 400);
         asettelu.setAlignment(Pos.CENTER);
         asettelu.setVgap(10);
         asettelu.setHgap(10);
         asettelu.setPadding(new Insets(20, 20, 20, 20));
 
-        // 1.4 luodaan itse näkymä ja asetetaan asettelu siihen
         Scene salasanaNakyma = new Scene(asettelu);
 
 
-        // 2. Luodaan tervetuloa-tekstin näyttämiseen käytetty näkymä
         Label tervetuloaTeksti = new Label("Tervetuloa, tästä se alkaa!");
 
         StackPane tervetuloaAsettelu = new StackPane();
